322ac40322f4ef3d2c43f221d017ce79eca11e59
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2015-11-12 12:45:34 +0530

    Add DELETE todos route'

--- server.js
+++ server.js
@@ -53,6 +53,19 @@ app.post('/todos', function(req, res){
 
 });
 
+//DELETE /todos/:id
+
+app.delete('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if(!matchedTodo){
+		res.status(404).json({"error" : "No item found with that ID"});
+	} else {
+		todos = _.without(todos,matchedTodo);
+		res.json(matchedTodo);
+	}
+});
 
 app.listen (PORT, function(){
 	console.log('Express listening on port: ' + PORT);
