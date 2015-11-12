b233e856bc8a6d59a59079aac41a150a323f3b1f
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2015-11-12 13:17:30 +0530

    add PUT request

--- server.js
+++ server.js
@@ -67,6 +67,43 @@ app.delete('/todos/:id', function(req, res){
 	}
 });
 
+// PUT /todos/:id
+app.put ('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	if(!matchedTodo){
+		return res.status(404).json({"error" : "No item found to update with that ID"});
+	} 
+
+	//body.hasOwnProperty('completed')
+
+	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		validAttributes.completed = body.completed;
+	} else if(body.hasOwnProperty('completed')) {
+		// Bad
+		return res.status(400).send('Error is delete.... property --> completed');
+	} else {
+		// Never provided Attribute, no problem here
+	}
+
+	if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+		validAttributes.description = body.description;
+	} else if(body.hasOwnProperty('description')) {
+		// Bad
+		return res.status(400).send('Error is delete.... property --> description');
+	}
+
+	///Doing actual update of the values as Objects are passed by Reference
+	/// No need to explicitly update the TODO object array
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
+
+
+});
+
 app.listen (PORT, function(){
 	console.log('Express listening on port: ' + PORT);
 });
